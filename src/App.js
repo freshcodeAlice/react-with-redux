364005599cf9c05e358968f50a1662f10fb9f20c
freshcodeAlice <freshjs.ofte@gmail.com>
2022-06-06 17:55:32 +0300

    useReducer remember

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import './App.css'
+import './App.css';
+import { useReducer } from 'react';
 
-function App () {
-  return <div className='App'></div>
+function reducer (state, action) {
+  switch (action.type) {
+    case 'ADD_ONE': {
+      return {};
+    }
+    case 'SUB_ONE': {
+      return {};
+    }
+  }
+  return state;
 }
 
-export default App
+const App = props => {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0
+  });
+
+  const increment = () => {
+    dispatch({
+      type: 'ADD_ONE',
+      count: state.count + 1
+    });
+  };
+
+  const decrement = () => {
+    dispatch({
+      type: 'SUB_ONE',
+      count: state.count - 1
+    });
+  };
+
+  return (
+    <div className='App'>
+      <h1>{state.count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
+};
+
+export default App;
