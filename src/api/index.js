d0826981213f0757290c82a3d9971ad4d20dd63d
freshcodeAlice <freshjs.ofte@gmail.com>
2022-06-09 18:02:25 +0300

    LoadMore

--- src/api/index.js
+++ src/api/index.js
@@ -6,5 +6,5 @@ const httpClient = axios.create({
 });
 
 export const createUser = data => httpClient.post('/users', data);
-export const getUsers = ({ offset, limit }) =>
+export const getUsers = ({ offset = 0, limit = 5 }) =>
   httpClient.get(`/users?${queryString({ offset, limit })}`);
--- src/components/UsersList/index.js
+++ src/components/UsersList/index.js
@@ -6,16 +6,20 @@ const UsersList = props => {
   const { users, isFetching, error } = props;
 
   useEffect(() => {
-    props.getUsersRequestAction({
-      limit: 5,
-      offset: 0
-    });
+    props.getUsersRequestAction({});
   }, []);
+
+  const loadMore = () => {
+    props.getUsersRequestAction({
+      offset: users.length
+    });
+  };
 
   return (
     <div>
       {isFetching && <p>Loading....</p>}
       {error && <p>Some error</p>}
+      <button>Load More Users</button>
       {users
         ? users.map(u => {
             return <li key={u.id}>{JSON.stringify(u)}</li>;
